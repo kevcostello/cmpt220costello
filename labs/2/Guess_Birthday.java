1d831571fe25695c64ea8fc8bcbccd9d36d08898
Kevin Costello <kcostello56@gmail.com>
2017-02-07 17:06:57 -0500

    Add files via upload

---
+++ labs/2/Guess_Birthday.java
@@ -0,0 +1,95 @@
+/* Kevin Costello, Professor Rivas, Software Dev 1 CMPT 220*/
+/* February 7, 2016, Lab 2 Question 4.10*/
+/* This program will guess what day is your birthday */
+import java.util.Scanner;
+
+public class Guess_Birthday{
+  public static void main(String[] args){
+    
+	//Each character set
+	String set1= 
+	  " 1  3  4  7\n" +
+	  " 9 11 13 15\n" +
+	  "17 19 21 23\n" +
+	  "25 27 29 31";
+	
+	String set2 =
+	  " 2  3  6  7\n" +
+	  "10 11 14 15\n" +
+	  "18 19 22 23\n" +
+	  "26 27 30 31";
+	  
+	String set3 =
+	  " 4  5  6  7\n" +
+	  "12 13 14 15\n" +
+	  "20 21 22 23\n" +
+	  "28 29 30 31";
+		
+	String set4 = 
+	  " 8  9 10 11\n" +
+	  "12 13 14 15\n" +
+	  "24 25 26 27\n" +
+	  "28 29 30 31";
+		
+	String set5 = 
+	  "16 17 18 19\n" +
+	  "20 21 22 23\n" +
+	  "24 25 26 27\n" +
+	  "28 29 30 31";
+	
+	int day = 0;
+	
+	//Create a Scanner
+	Scanner input = new Scanner(System.in);
+	
+
+    // Prompt the user to enter Y or N to to each character set
+	System.out.print("Is your birthday in set1?\n");
+	System.out.print(set1);
+	System.out.print("\nEnter 'Y' for Yes or 'N' for No: ");
+	String s1 = input.nextLine();
+	char answer = s1.charAt(0);
+	
+	if (Character.toUpperCase(answer) == 'Y')
+	  day += 1;
+		
+	System.out.print("Is your birthday in set2?\n");
+	System.out.print(set2);
+	System.out.print("\nEnter 'Y' for Yes or 'N' for No: ");
+	String s2 = input.nextLine();
+	answer = s2.charAt(0);
+	
+	if (Character.toUpperCase(answer) == 'Y')
+	  day += 2;
+	
+	System.out.print("Is your birthday in set3?\n");
+	System.out.print(set3);
+	System.out.print("\nEnter 'Y' for Yes or 'N' for No: ");
+	String s3 = input.nextLine();
+	answer = s3.charAt(0);
+	
+	if (Character.toUpperCase(answer) == 'Y')
+	  day += 4;
+	
+	System.out.print("Is your birthday in set4?\n");
+	System.out.print(set4);
+	System.out.print("\nEnter 'Y' for Yes or 'N' for No: ");
+	String s4 = input.nextLine();
+	answer = s4.charAt(0);
+	
+	if (Character.toUpperCase(answer) == 'Y')
+	  day += 8;
+	
+	System.out.print("Is your birthday in set5?\n");
+    System.out.print(set5);
+	System.out.print("\nEnter 'Y' for Yes or 'N' for No: ");
+	String s5 = input.nextLine();
+	answer = s5.charAt(0);
+	
+	if (Character.toUpperCase(answer) == 'Y')
+	 day += 16;
+	
+    //Adds the day variable to create a total, selecting the persons birthday
+	System.out.println("\nYour birthday is " + day + "!");
+  }
+}  
