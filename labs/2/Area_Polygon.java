e50dbb5ac05dd67e9f2f3b0140f577f3db355845
Kevin Costello <kcostello56@gmail.com>
2017-02-14 19:57:59 -0500

    Update Area_Polygon.java

--- labs/2/Area_Polygon.java
+++ labs/2/Area_Polygon.java
@@ -5,6 +5,8 @@
  * assignment: lab 2, question 4.5
  * due date: February 14, 2016
  * version: 1.0
+ *
+ * This program will calculate the area of a polygon with a given length from the center to a vertex and the numbe of sides.
  */
 
 import java.util.Scanner;
