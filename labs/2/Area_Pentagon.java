6f9bd4a9ba73cb9b9038105f11f60528a85bfb71
Kevin Costello <kcostello56@gmail.com>
2017-02-14 19:56:57 -0500

    Update Area_Pentagon.java

--- labs/2/Area_Pentagon.java
+++ labs/2/Area_Pentagon.java
@@ -3,8 +3,10 @@
  * author: Kevin Costello
  * course: CMPT 220
  * assignment: lab 2, question 4.1
- * due date: February 14, 2016
+ * due date: February 7, 2017
  * version: 1.0
+ *
+ * This program will calculate the area of a pentagon, using a given length from the center to a vertex
  */
 
 import java.util.Scanner;
