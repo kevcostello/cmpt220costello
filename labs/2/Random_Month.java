d0ecd9eca9265e4fe438daa95e2dc0372a5d9a8c
Kevin Costello <kcostello56@gmail.com>
2017-02-15 17:55:51 -0500

    Update Random_Month.java

--- labs/2/Random_Month.java
+++ labs/2/Random_Month.java
@@ -10,33 +10,33 @@
  
 public class Random_Month {
   public static void main (String[] args){
-	System.out.println("A random month will appear below.");
+    System.out.println("A random month will appear below.");
 	
-	int randomnum = (int)(Math.random() * 13);
-	if ( randomnum == 1 )
-	  System.out.println("The month is January");
-	else if ( randomnum == 2 )
-	  System.out.println("The month is February");
-	else if ( randomnum == 3 )
-	  System.out.println("The month is March");
-    else if ( randomnum == 4 )
-	  System.out.println("The month is April");
-    else if ( randomnum == 5 )
-	  System.out.println("The month is May");
-    else if ( randomnum == 6 )
-	  System.out.println("The month is June");
-    else if ( randomnum == 7 )
-	  System.out.println("The month is July");
-    else if ( randomnum == 8 )
-	  System.out.println("The month is August");
-    else if ( randomnum == 9 )
-	  System.out.println("The month is September");
-    else if ( randomnum == 10 )
-	  System.out.println("The month is October");
-    else if ( randomnum == 11 )
-	  System.out.println("The month is November");
-    else
-	  System.out.println("The month is December");
+      int randomnum = (int)(Math.random() * 13);
+      if ( randomnum == 1 )
+        System.out.println("The month is January");
+      else if ( randomnum == 2 )
+        System.out.println("The month is February");
+      else if ( randomnum == 3 )
+        System.out.println("The month is March");
+      else if ( randomnum == 4 )
+        System.out.println("The month is April");
+      else if ( randomnum == 5 )
+        System.out.println("The month is May");
+      else if ( randomnum == 6 )
+        System.out.println("The month is June");
+      else if ( randomnum == 7 )
+        System.out.println("The month is July");
+      else if ( randomnum == 8 )
+        System.out.println("The month is August");
+      else if ( randomnum == 9 )
+        System.out.println("The month is September");
+      else if ( randomnum == 10 )
+        System.out.println("The month is October");
+      else if ( randomnum == 11 )
+        System.out.println("The month is November");
+      else
+        System.out.println("The month is December");
   }
 }
 
