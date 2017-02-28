a01d791a96a8757b49a43eafdacbcee222aa3628
Kevin Costello <kcostello56@gmail.com>
2017-02-28 18:04:47 -0500

    Update String_Counter.java

--- labs/4/String_Counter.java
+++ labs/4/String_Counter.java
@@ -14,18 +14,18 @@ public class String_Counter{
     Scanner input = new Scanner(System.in);
 	
 	//Prompts user to enter any series of letters or sentences
-	System.out.print("Please enter any number of letters:");
-	String letters = input.nextLine();
-	System.out.print("The number of letters is: " + countLetters(letters));
+      System.out.print("Please enter any number of letters:");
+      String letters = input.nextLine();
+      System.out.print("The number of letters is: " + countLetters(letters));
   }
   //Method counts the number of strings
   public static int countLetters(String s){
-	int counter = 0;
-	for (int i = 0; i < s.length(); i++){
-	  //Increases the value of the counter by 1 each time a char is located
-	  if (Character.isLetter(s.charAt(i)))
-		counter++;
-	}
+    int counter = 0;
+      for (int i = 0; i < s.length(); i++){
+        //Increases the value of the counter by 1 each time a char is located
+	if (Character.isLetter(s.charAt(i)))
+          counter++;
+      }
 	
 	return counter;
 	  
