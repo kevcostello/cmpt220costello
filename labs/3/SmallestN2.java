954d72da1dff86697e54f9a7ffe0ab249252f4e8
Kevin Costello <kcostello56@gmail.com>
2017-02-28 21:59:16 -0500

    Update SmallestN2.java

--- labs/3/SmallestN2.java
+++ labs/3/SmallestN2.java
@@ -12,13 +12,13 @@
 public class SmallestN2{
   public static void main (String[] args){
 	  
-	//Initialize all variables
-	int n = 1;
+    //Initialize all variables
+    int n = 1;
 	
-	//Increases the value of n by 1 each time its cube isnt greater than 12,000
-	while ((int) Math.pow (n, 3) < 12000 ){
-      ++n;
-	}
-	System.out.print ("The smallest number cubed greater than 12000 is: " + n);
+      //Increases the value of n by 1 each time its cube isnt greater than 12,000
+      while ((int) Math.pow (n, 3) < 12000 ){
+        ++n;
+      }
+      System.out.print ("The smallest number cubed greater than 12000 is: " + n);
   }
 }
